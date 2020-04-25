7133ee21c2d9a8be345d7647647b77f30326f499
tromgreg <grelarson1@gmail.com>
2020-04-24 22:07:38 -0700

    this isn't easy

--- JS files/quiz.js
+++ JS files/quiz.js
@@ -1,12 +1,120 @@
-function submitName(event){
-    event.preventDefault();
-    let name = document.forms ["name_form"] ["name"] . value;
-    
-    //save name
-    sessionStorage.setItem("name", name);
+let questions =  [
+{
+    id: 1,
+    question: "what is a horse?",
+    answer: "A four legged animal",
+    options:[
+        "a man with a long arm",
+        "a bad guy in a comic book",
+        "a four legged animal",
+        "none of the above"
+    ]
+},
+{
+    id: 2,
+    question: "what is a horse?",
+    answer: "A four legged animal",
+    options:[
+        "a man with a long arm",
+        "a bad guy in a comic book",
+        "a four legged animal",
+        "none of the above"
+    ]
+},
+{
+    id: 3,
+    question: "what is a horse?",
+    answer: "A four legged animal",
+    options:[
+        "a man with a long arm",
+        "a bad guy in a comic book",
+        "a four legged animal",
+        "none of the above"
+    ]
+},
+{
+    id: 4,
+    question: "what is a horse?",
+    answer: "A four legged animal",
+    options:[
+        "a man with a long arm",
+        "a bad guy in a comic book",
+        "a four legged animal",
+        "none of the above"
+    ]
+},
+{
+    id: 5,
+    question: "What is the full form of E-mail",
+    answer: "Electronic Mail",
+    options: [
+        "Electronic Mail",
+        "Electric Mail",
+        "Engine Mail",
+        "None of these"
+    ]
+  }
+];
 
-    location.href = "quiz.html";
+let question_count = 0;
     
-    console.log(name);
+let points = 0;
 
+window.onload = function(){
+    show(question_count);
+};
+
+function next(){
+
+    //last question
+    if (question_count == questions.length - 1){
+        sessionStorage.setItem("time", time);
+        clearInterval(mytime);
+        location.href="end.html";
+    }
+    console.log(question_count);
+
+    let user_answer = document.querySelector("li.option.active").innerHTML;
+
+    //right or wrong answer checker
+
+    if (user_answer == questions[question_count].answer){
+        points += 10;
+        sessionStorage.setItem("points", points);
+    }
+
+    console.log(points);
+
+    question_count++;
+    show(question_count);
+}
+
+function show(count){
+    let question = document.getElementById("questions");
+    let [first, second, third, fourth] = questions[count].options;
+
+    question.innerHTML = `
+    <h2>Q${count = 1}. ${questions [count].question}</h2>
+        <ul class="option_group">
+        <li class="option">${first}</li>
+        <li class="option">${second}</li>
+        <li class="option">${third}</li>
+        <li class="option">${fourth}</li>
+        </ul>`;
+
+ toggleActive();
+}
+
+function toggleActive(){
+    let option = document.querySelectorAll("li.option");
+    for (let i = 0; i < option.length; i++){
+        option[i].onclick = function(){
+            for (let i = 0; i < option.length; i++){
+                if (option[i].classList.contains("active")){
+                    option[i].classList.remove("active");
+                }
+            }
+            option[i].classList.add("active");
+        };
+    }
 }
--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Homework for week 4
 
 20APR2020:
     Stared initial working of the project. This includes the HTML for a screen that the user can enter their name into and then that name will save (through session storage) for the remainder of the test. upon the submission of the user's name the quiz is triggered through submit even to href of the quiz.html. Initial css started. I will play around flexbox and grid during this project and also utilize a fair amount trying to hone building js functions with for loops. 
+
+23APR2020:
+Working out the bugs and trying to get the timer finished. as usual i am having difficulty trying to organize my work and then my computer deadlocked. After a re-image to another SSD I mostly had to start again from my google drive save. 
+
+24APR2020:
+
+further debugging. Cannot figure out what i am doing wrong with my toggleactive question so i can advance to the next question. 
